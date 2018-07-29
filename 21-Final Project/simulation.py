15dbaaad278e17412fb77c7f6db82c751e9c30d2
Asoka <asokawotulo@Asokas-MacBook-Pro.local>
2018-07-29 22:46:27 +0700

    Cleanup Code

--- 21-Final Project/simulation.py
+++ 21-Final Project/simulation.py
@@ -1,12 +1,12 @@
 from pendulum import Pendulum
-from numpy import sin, cos, arange
 from matplotlib import pylab
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import scipy.integrate as integrate
 
 # Physics Constants
-gravity = 100
+gravity = 9.81
 
 # User Input Constants for Pendulum
 pendulum = Pendulum()
@@ -41,31 +41,31 @@ def dydx(state, time):
 
 	angle_diff = angle_one - angle_two
 	mass_sum = mass_one + mass_two
-	division_one = length_one*(mass_sum) - mass_two*length_one*cos(angle_diff)**2
-	division_two = length_two*(mass_sum) - mass_two*length_two*cos(angle_diff)**2
+	division_one = length_one*(mass_sum) - mass_two*length_one*np.cos(angle_diff)**2
+	division_two = length_two*(mass_sum) - mass_two*length_two*np.cos(angle_diff)**2
 
 	# Derivative of angular velocity
-	omega_one_der = (-mass_two*length_one*angle_one_der**2*sin(angle_diff)*cos(angle_diff) + gravity*mass_two*sin(angle_two)*cos(angle_diff) - mass_two*length_two*angle_two_der**2*sin(angle_diff) - (mass_sum)*gravity*sin(angle_one))/division_one
-	omega_two_der = (mass_two*length_two*angle_two_der**2*sin(angle_diff)*cos(angle_diff) + gravity*sin(angle_one)*cos(angle_diff)*(mass_sum) + length_one*angle_one_der**2*sin(angle_diff)*(mass_sum) - gravity*sin(angle_two)*(mass_sum))/division_two
+	omega_one_der = (-mass_two*length_one*angle_one_der**2*np.sin(angle_diff)*np.cos(angle_diff) + gravity*mass_two*np.sin(angle_two)*np.cos(angle_diff) - mass_two*length_two*angle_two_der**2*np.sin(angle_diff) - (mass_sum)*gravity*np.sin(angle_one))/division_one
+	omega_two_der = (mass_two*length_two*angle_two_der**2*np.sin(angle_diff)*np.cos(angle_diff) + gravity*np.sin(angle_one)*np.cos(angle_diff)*(mass_sum) + length_one*angle_one_der**2*np.sin(angle_diff)*(mass_sum) - gravity*np.sin(angle_two)*(mass_sum))/division_two
 
 	return [angle_one_der, omega_one_der, angle_two_der, omega_two_der]
 
 # Time Constants
 start_time = 0.0
 total_time = float(input("Insert total time: ")) # Total time (s)
-incre_time = 0.01 # Increment Time (s)
-array_time = arange(start_time, total_time, incre_time)
+incre_time = 0.001 # Increment Time (s)
+array_time = np.arange(start_time, total_time, incre_time)
 
 # Grid and axis
 max_space = length_one + length_two + 0.5
 fig = plt.figure(facecolor="#000000")
 fig.canvas.set_window_title('Double Pendulum')
 ax = fig.add_subplot(111, autoscale_on=False, xlim=(-max_space, max_space), ylim=(-max_space, max_space))
-ax.text(0.01, max_space, "D O U B L E   P E N D U L U M", ha="left", color="white", fontsize=12)
+ax.text(0.01, max_space, "D O U B L E   P E N D U L U M", ha="left", color="#000000", fontsize=12)
 
 # Time Template
 time_template = 'Time = %.2fs'
-time_text = ax.text(0.01, 0.925, '', transform=ax.transAxes, color="white")
+time_text = ax.text(0.01, 0.925, '', transform=ax.transAxes, color="#000000")
 
 # Pendulum Visible
 if pendulum_visible.lower() == "y":
@@ -79,10 +79,10 @@ else:
 integration = integrate.odeint(dydx, state, array_time)
 
 # Find xy positions of Pendulum 1 & 2
-x_one =  length_one*sin(integration[:, 0])
-x_two =  length_two*sin(integration[:, 2]) + x_one
-y_one = -length_one*cos(integration[:, 0])
-y_two = -length_two*cos(integration[:, 2]) + y_one
+x_one =  length_one*np.sin(integration[:, 0])
+x_two =  length_two*np.sin(integration[:, 2]) + x_one
+y_one = -length_one*np.cos(integration[:, 0])
+y_two = -length_two*np.cos(integration[:, 2]) + y_one
 
 # Initial plot
 def init():
@@ -97,10 +97,10 @@ def animate(i):
 
 	# Trailing 
 	if trailing == "y" and all_trail == "n":
-		if i - 400 < 0:
+		if i - 1000 < 0:
 			trail = 0
 		else:
-			trail = i-400
+			trail = i-1000
 		trace.set_data(x_two[trail:i], y_two[trail:i])
 	elif trailing == "y":
 		trace.set_data(x_two[:i], y_two[:i])
@@ -113,7 +113,7 @@ def animate(i):
 	return line, time_text
 
 # Animate Function
-ani = animation.FuncAnimation(fig, animate, arange(1, len(integration)), interval=5, init_func=init, repeat=False)
+ani = animation.FuncAnimation(fig, animate, np.arange(1, len(integration)), interval=1, init_func=init, repeat=False)
 
 # Turn Axis Off & Show Plot
 plt.axis("off")
